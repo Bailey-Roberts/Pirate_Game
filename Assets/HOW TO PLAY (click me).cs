8f9897327e717b1841e4f64976f4128676aaa628
Bailey Roberts <bailey2.roberts@live.uwe.ac.uk>
2024-05-04 05:56:20 +0100

    HTP + CatBorg Studio Files

---
+++ Assets/HOW TO PLAY (click me).cs
@@ -0,0 +1,7 @@
+// HOW TO PLAY!
+//
+// Look around with the Mouse
+// Move with WASD & Jump with Space
+// 'E' to Pick Up items
+// 'Q' to Drop items
+// (you must be close proximity before being able to pick up an item)
